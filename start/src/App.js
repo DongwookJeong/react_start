0bf7f588efa89ca7bdc7992862214aca6768184a
DongwookJeong <nolhoon211@gmail.com>
2023-03-15 14:52:33 +0900

    if문은 JSX 내부에서 X, 대신 조건부 연산자(삼항 연산자)

--- start/src/App.js
+++ start/src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = "리액트";
   return (
     <div>
-      <h1>리액트를 공부하는중입니다.</h1>
-      <h2>부모 요소에 감싸지 않으니 바로 에러를 친절하게 바로 보여주네</h2>
+      {name === '리액트' ? (<h1>리액트입니다.</h1>) : (<h2>리액트가 아닙니다.</h2>)}
     </div>
   );
 }
