58b015aa190e69881179dbe74eccb02804012ca5
DongwookJeong <nolhoon211@gmail.com>
2023-03-19 20:23:34 +0900

    class 내부에서 defaultProps, propTypes 지정/ 사용해야하는 이유 해당 컴포넌트에 어떤 props가 필요한지 파악하기 쉽기 때문 협업 능률에 좋다.

--- start/src/App.js
+++ start/src/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="react" favoriteNumber={4}>리액트</MyComponent>;
+  return <MyComponent name="react" favoriteNumber={17}>리액트</MyComponent>;
 };
 
 export default App;
--- start/src/MyComponent.js
+++ start/src/MyComponent.js
@@ -2,24 +2,23 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
-    render() {
-        const {name, favoriteNumber, children}= this.props;
+    static defaultProps = {
+        name : '기본 이름'
+    };
+    static propTypes = {
+        name : PropTypes.string,
+        favoriteNumber: PropTypes.number.isRequired
+    }
+    render(){
+        const {name, favoriteNumber, Children} = this.props;
         return (
             <>
             안녕하세요, 제 이름은 {name}입니다. <br/>
-            children 값은 {children}입니다. <br/>
+            children 값은 {Children}입니다. <br/>
             제가 좋아하는 숫자는 {favoriteNumber}입니다.
             </>
         )
     }
 }
 
-MyComponent.defaultProps = {
-    name : '삶'
-};
-
-MyComponent.propTypes = {
-    name : PropTypes.string,
-    favoriteNumber : PropTypes.number.isRequired
-}
 export default MyComponent;
