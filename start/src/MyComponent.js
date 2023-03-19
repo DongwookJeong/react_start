0e39cce5b2e23f1253c198d146b514a3f824416e
DongwookJeong <nolhoon211@gmail.com>
2023-03-19 15:03:38 +0900

    children

--- start/src/MyComponent.js
+++ start/src/MyComponent.js
@@ -1,5 +1,10 @@
 const MyComponent = props => {
-    return <div>지금은 {props.name}을 공부중입니다.</div>
+    return (
+    <div>
+        지금은 {props.name}을 공부중입니다. <br/>
+        children 값은 {props.children}입니다.
+    </div>
+    );
 };
 
 MyComponent.defaultProps = {
