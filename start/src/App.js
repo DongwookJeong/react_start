da4fe04863ecb34adb732fc7668960bb3d13b762
DongwookJeong <nolhoon211@gmail.com>
2023-03-19 14:43:43 +0900

    defaultProps 설정 방법

--- start/src/App.js
+++ start/src/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name = "react"/>;
+  return <MyComponent/>;
 };
 
 export default App;
--- start/src/MyComponent.js
+++ start/src/MyComponent.js
@@ -2,4 +2,8 @@ const MyComponent = props => {
     return <div>지금은 {props.name}을 공부중입니다.</div>
 };
 
+MyComponent.defaultProps = {
+    name : '삶'
+};
+
 export default MyComponent;
