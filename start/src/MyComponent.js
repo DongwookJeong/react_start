2a20a880b39514ce45d197bf7bf41bfa6485be9e
DongwookJeong <nolhoon211@gmail.com>
2023-03-19 17:18:47 +0900

    파라미터 부분

--- start/src/MyComponent.js
+++ start/src/MyComponent.js
@@ -1,12 +1,11 @@
-const MyComponent = props => {
-    const {name, children} = props;
+const MyComponent = ({name, children}) => {
     return (
-    <div>
-        지금은 {name}을 공부중입니다. <br/>
+        <>
+        지금은 {name}을 공부중입니다.<br/>
         children 값은 {children}입니다.
-    </div>
-    );
-};
+        </>
+    )
+}
 
 MyComponent.defaultProps = {
     name : '삶'
