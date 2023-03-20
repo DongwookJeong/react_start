acefa700a1d010ef4c4c4b570ead72bf3ed728bd
DongwookJeong <nolhoon211@gmail.com>
2023-03-20 19:23:13 +0900

    state에 input 값 담고 검증, comment 값 공백

--- start/src/EventPractice.js
+++ start/src/EventPractice.js
@@ -1,20 +1,38 @@
 import { Component } from "react";
 
-class EventPractice extends Component{
-    render(){
+class EventPractice extends Component {
+
+    state = {
+        message: ''
+    }
+
+    render() {
         return (
             <div>
                 <h1>
                     이벤트 연습
                 </h1>
-                <input type="text"
-                name="message"
-                placeholder="any"
-                onChange={
-                    (e)=>{console.log(e.target.value);
-                }
-                }
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="any"
+                    value={this.state.message}
+                    onChange={
+                        (e) => {
+                            this.setState({
+                                message: e.target.value
+                            })
+                        }
+                    }
                 />
+                <button onClick={
+                    ()=>{
+                        alert(this.state.message)
+                        this.setState({
+                            message: ''
+                        })
+                    }
+                }>확인</button>
             </div>
         )
     }
