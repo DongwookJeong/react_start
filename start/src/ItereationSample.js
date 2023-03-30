4c7b85f1bd7d4adb686d859a81792ca23bae5f08
DongwookJeong <nolhoon211@gmail.com>
2023-03-31 00:16:28 +0900

    더블 클릭으로 항목 삭제 이벤트

--- start/src/ItereationSample.js
+++ start/src/ItereationSample.js
@@ -20,8 +20,15 @@ const IterationSample = () => {
         setNames(nextNames) // ! namse 값을 업데이트 한다.
         setInPutText('') // ! inputText를 비운다.
     }
-    
-    const namesList = names.map(name => <li key={name.id}>{name.text}</li>)
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id)
+        setNames(nextNames)
+    }
+
+    const namesList = names.map(name => 
+        <li key={name.id} onDoubleClick={()=>onRemove(name.id)}>
+            {name.text}
+        </li>)
     return (
         <>
             <input value = {inputText} onChange={onChange}/>
